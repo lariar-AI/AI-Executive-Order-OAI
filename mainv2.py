5b27c4ddfc31d3755b8d86179024439d27af0178
lariar <randy.lariar@gmail.com>
2023-11-09 19:13:50 +0000

    try a different template

--- mainv2.py
+++ mainv2.py
@@ -1,64 +1,123 @@
-# https://github.com/Doomcow500/StreamlitAI/blob/main/main.py
+# https://github.com/tractorjuice/STGPT/blob/main/app.py
+# Importing required packages
 import streamlit as st
 import openai
 import uuid
 import time
+
 from openai import OpenAI
-
-# Constants
-MODEL = "gpt-4-1106-preview"
-RETRY_LIMIT = 3
-SHORT_DELAY = 1
-LONG_DELAY = 3
-
-# Initialize OpenAI client
 client = OpenAI()
 
-def initialize_session_state():
-    if "session_id" not in st.session_state:
-        st.session_state.session_id = str(uuid.uuid4())
-    if "run" not in st.session_state:
-        st.session_state.run = {"status": None}
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    if "retry_error" not in st.session_state:
-        st.session_state.retry_error = 0
+#MODEL = "gpt-3.5-turbo"
+#MODEL = "gpt-3.5-turbo-0301"
+#MODEL = "gpt-3.5-turbo-0613"
+#MODEL = "gpt-3.5-turbo-1106"
+#MODEL = "gpt-3.5-turbo-16k"
+#MODEL = "gpt-3.5-turbo-16k-0613"
+#MODEL = "gpt-4"
+#MODEL = "gpt-4-0613"
+#MODEL = "gpt-4-0613"
+#MODEL = "gpt-4-32k-0613"
+MODEL = "gpt-4-1106-preview"
+#MODEL = "gpt-4-vision-preview"
 
-def initialize_assistant():
-    if "assistant" not in st.session_state:
-        openai.api_key = st.secrets["OPENAI_API_KEY"]
-        st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT"])
-        st.session_state.thread = client.beta.threads.create(metadata={'session_id': st.session_state.session_id})
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
 
-def handle_run_status():
-    if hasattr(st.session_state.run, 'status'):
-        if st.session_state.run.status == "running":
-            display_message('assistant', "Thinking ......", SHORT_DELAY)
-        elif st.session_state.run.status == "failed":
-            st.session_state.retry_error += 1
-            if st.session_state.retry_error < RETRY_LIMIT:
-                display_message('assistant', "Run failed, retrying ......", LONG_DELAY)
+if "run" not in st.session_state:
+    st.session_state.run = {"status": None}
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "retry_error" not in st.session_state:
+    st.session_state.retry_error = 0
+
+st.set_page_config(page_title="Learn Wardley Mapping")
+st.sidebar.title("Learn Wardley Mapping")
+st.sidebar.divider()
+st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
+st.sidebar.markdown("Current Version: 0.0.3")
+st.sidebar.markdown("Using gpt-4-1106-preview API")
+st.sidebar.markdown(st.session_state.session_id)
+st.sidebar.divider()
+
+if "assistant" not in st.session_state:
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+    # Load the previously created assistant
+    st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT"])
+
+    # Create a new thread for this session
+    st.session_state.thread = client.beta.threads.create(
+        metadata={
+            'session_id': st.session_state.session_id,
+        }
+    )
+ 
+# If the run is completed, display the messages
+elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == "completed":
+    # Retrieve the list of messages
+    st.session_state.messages = client.beta.threads.messages.list(
+        thread_id=st.session_state.thread.id
+    )
+
+    # Display messages
+    for message in reversed(st.session_state.messages.data):
+        if message.role in ["user", "assistant"]:
+            with st.chat_message(message.role):
+                for content_part in message.content:
+                    message_text = content_part.text.value
+                    st.markdown(message_text)
+
+if prompt := st.chat_input("How can I help you?"):
+    with st.chat_message('user'):
+        st.write(prompt)
+
+    # Add message to the thread
+    st.session_state.messages = client.beta.threads.messages.create(
+        thread_id=st.session_state.thread.id,
+        role="user",
+        content=prompt
+    )
+
+    # Do a run to process the messages in the thread
+    st.session_state.run = client.beta.threads.runs.create(
+        thread_id=st.session_state.thread.id,
+        assistant_id=st.session_state.assistant.id,
+    )
+    if st.session_state.retry_error < 3:
+        time.sleep(1) # Wait 1 second before checking run status
+        st.rerun()
+                    
+# Check if 'run' object has 'status' attribute
+if hasattr(st.session_state.run, 'status'):
+    # Handle the 'running' status
+    if st.session_state.run.status == "running":
+        with st.chat_message('assistant'):
+            st.write("Thinking ......")
+        if st.session_state.retry_error < 3:
+            time.sleep(1)  # Short delay to prevent immediate rerun, adjust as needed
+            st.rerun()
+
+    # Handle the 'failed' status
+    elif st.session_state.run.status == "failed":
+        st.session_state.retry_error += 1
+        with st.chat_message('assistant'):
+            if st.session_state.retry_error < 3:
+                st.write("Run failed, retrying ......")
+                time.sleep(3)  # Longer delay before retrying
+                st.rerun()
             else:
                 st.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
-        elif st.session_state.run.status != "completed":
-            st.session_state.run = client.beta.threads.runs.retrieve(thread_id=st.session_state.thread.id, run_id=st.session_state.run.id)
-            if st.session_state.retry_error < RETRY_LIMIT:
-                time.sleep(LONG_DELAY)
-                st.rerun()
 
-def display_message(role, message, delay):
-    with st.chat_message(role):
-        st.write(message)
-    if st.session_state.retry_error < RETRY_LIMIT:
-        time.sleep(delay)
-        st.rerun()
-
-# Main
-initialize_session_state()
-initialize_assistant()
-handle_run_status()
-
-
-if prompt := st.chat_input('Ask a question about the AI Executive Order'):
-    display_message("user",prompt,1)
-    st.session_state.run.status = "go"
+    # Handle any status that is not 'completed'
+    elif st.session_state.run.status != "completed":
+        # Attempt to retrieve the run again, possibly redundant if there's no other status but 'running' or 'failed'
+        st.session_state.run = client.beta.threads.runs.retrieve(
+            thread_id=st.session_state.thread.id,
+            run_id=st.session_state.run.id,
+        )
+        if st.session_state.retry_error < 3:
+            time.sleep(3)
+            st.rerun()
